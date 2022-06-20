c44d2308a413f2aee2c2698d679ea92ccbd9c7e3
Roman911 <roma-lysyk@ukr.net>
2022-06-20 21:54:54 +0300

    server and client: update posts

--- client/modules/News/Components/NewsPage.component.tsx
+++ client/modules/News/Components/NewsPage.component.tsx
@@ -1,32 +1,39 @@
 import { IPost } from '../../../typesScript/post'
 import { IUserData } from '../../../typesScript/user'
 import React from "react"
-import { Box, Container, Grid, Stack, Typography } from '@mui/material'
+import { Box, Container, Grid, Pagination, PaginationItem, Stack, Typography } from '@mui/material'
 import { PreviewCard } from '../../'
 import { PreviewCardSKeleton } from '../../PreviewCard/Components'
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 interface IProps {
   loading: boolean | undefined
   error: any
   userData: IUserData | null
   data?: {
-    page: number
-    total_pages: number
-    total_posts: number
-    posts: IPost[]
+    postsAndParams: {
+      page: number
+      total_pages: number
+      total_posts: number
+      posts: IPost[]
+    }
   }
 }
 
 export const NewsPageComponent: React.FC<IProps> = ({ loading, error, data }) => {
   const postsVievs = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-  const post = data?.posts.map(i => {
-    return <PreviewCard key={i._id} item={i} usedId={'usedId'} md={4} />
+  const post = data?.postsAndParams.posts.map(i => {
+    return <PreviewCard key={i._id} item={i} usedId={'usedId'} md={3} />
   })
 
   const postsSkeleton = postsVievs.map(i => {
-    return <PreviewCardSKeleton key={i} md={4} />
+    return <PreviewCardSKeleton key={i} md={3} />
   })
+
+  //const page = parseInt(query.get('page') || '1', 10)
 
   return <Container maxWidth='xl' >
     <Typography marginTop={10} marginLeft={2} variant="h3">Новини</Typography>
@@ -41,14 +48,43 @@ export const NewsPageComponent: React.FC<IProps> = ({ loading, error, data }) =>
           </Typography>
         </Stack>
         <Grid container alignItems='center' marginTop={2} sx={{ flexDirection: { xs: 'column', sm: 'column', md: 'row' }, padding: { xs: '0', sm: '0', md: '0 10px' } }}>
-          {data?.posts ? post : postsSkeleton}
+          {data?.postsAndParams.posts ? post : postsSkeleton}
         </Grid>
       </Grid>
       <Grid item xs={2}>
-        <Typography variant='body2'>
-          Категорії
+        <Typography variant='h6'>
+          Області
+        </Typography>
+        <Typography variant='subtitle2'>
+          Львівська
+        </Typography>
+        <Typography variant='subtitle2'>
+          Рівненська
+        </Typography>
+        <Typography variant='subtitle2'>
+          Сумська
+        </Typography>
+        <Typography variant='subtitle2'>
+          Чернігівська
+        </Typography>
+        <Typography variant='subtitle2'>
+          Черкаська
+        </Typography>
+        <Typography variant='subtitle2'>
+          Закарпатьська
         </Typography>
       </Grid>
     </Grid>
+    <Stack spacing={2}>
+      <Pagination
+        count={10}
+        renderItem={(item) => (
+          <PaginationItem
+            components={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+            {...item}
+          />
+        )}
+      />
+    </Stack>
   </Container>
 }
