919ed5e134ed94bd0644ab18727fcc39654622e2
Roman911 <roma-lysyk@ukr.net>
2022-06-21 17:01:45 +0300

    server: Added update user. client: Update settings

--- client/modules/Maps/Components/Maps.component.tsx
+++ client/modules/Maps/Components/Maps.component.tsx
@@ -1,20 +1,28 @@
 import React from "react"
 import { GoogleMap, Marker, DirectionsRenderer, Circle, MarkerClusterer } from '@react-google-maps/api'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Paper, Typography } from '@mui/material'
 
+import mapStyles from '../styles/mapStyles'
 
 
 export const MapsComponents: React.FC = () => {
-  const center = React.useMemo(() => ({ lat: 43, lng: -80 }), [])
+  const center = React.useMemo(() => ({ lat: 49.026151, lng: 31.483070 }), [])
 
-  return <Grid container height='calc(100vh - 64px)' sx={{ border: '1px solid red' }}>
-    <Grid item xs={10} sx={{ border: '1px solid red' }}>
-      <GoogleMap zoom={10} center={center} mapContainerStyle={{ width: '100%', height: 'calc(100vh - 64px)' }}>
+  return <Grid container height='calc(100vh - 64px)'>
+    <Grid item xs={9}>
+      <GoogleMap
+      zoom={6}
+      center={center}
+      mapContainerStyle={{ width: '100%', height: 'calc(100vh - 64px)' }}
+      //options={{ styles: mapStyles }}
+      >
 
       </GoogleMap>
     </Grid>
-    <Grid item xs={2} sx={{ border: '1px solid red' }}>
-      13213
+    <Grid item xs={3}>
+      <Typography variant="h6">
+        Області
+      </Typography>
     </Grid>
   </Grid>
 }
